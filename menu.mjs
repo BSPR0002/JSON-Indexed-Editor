244ccfc5d835f0196c92fb45a24d8561c3a695ec
BSPR0002 <42113148+BSPR0002@users.noreply.github.com>
2025-02-01 06:23:06 +0800

    菜单更新

--- menu.mjs
+++ menu.mjs
@@ -1,40 +1,98 @@
 import { EVENT_LISTENERS, parse } from "/javascript/module/ArrayHTML.mjs";
-import showMenu from "/javascript/module/ContextMenu.mjs";
+import showMenu, { closeMenu } from "/javascript/module/ContextMenu.mjs";
 const menus = [], menuElement = document.getElementById("menu");
-var focused = false;
+var focusItem = null, expanded = false, keepFocus = false, keyboardMode = true;
 function renderMenu() {
 	menuElement.innerHTML = "";
 	// @ts-ignore
 	menuElement.appendChild(parse(menus.map(menuItemMapper)))
 }
-/**
- * 
- * @param {KeyboardEvent} event 
- */
-function preventTab(event) { if (!event.ctrlKey && event.shiftKey && event.altKey && event.key == "tab") event.preventDefault() }
-function onMenuFocus(e) {
-	console.log(e.type, document.activeElement, e)
-	focused = true;
-	document.addEventListener("keydown", preventTab);
-	menuElement.removeEventListener("focusin", onMenuFocus);
+function keyboardEvent(event) {
+	if (event.ctrlKey || event.shiftKey || event.altKey) return;
+	switch (event.key) {
+		case "tab": break;
+		case "ArrowRight": {
+			keepFocus = true;
+			const children = menuElement.children, currentIndex = focusItem.tabIndex;
+			// @ts-ignore
+			menuElement.children[currentIndex < children.length - 1 ? currentIndex + 1 : 0].focus();
+			break;
+		}
+		case "ArrowLeft": {
+			keepFocus = true;
+			const children = menuElement.children, currentIndex = focusItem.tabIndex;
+			// @ts-ignore
+			menuElement.children[currentIndex ? currentIndex - 1 : children.length - 1].focus();
+			break;
+		}
+		case "Escape":
+			focusItem.blur();
+			break;
+		default:
+			return;
+	}
+	event.preventDefault()
 }
-function onMenuBlur(e) {
-	console.log(e.type, document.activeElement, e)
-	focused = false;
-	document.removeEventListener("keydown", preventTab);
-	menuElement.addEventListener("focusin", onMenuFocus);
+function onMenuFocus() { document.addEventListener("keydown", keyboardEvent) }
+function onMenuBlur() {
+	focusItem = null;
+	document.removeEventListener("keydown", keyboardEvent);
 }
-function focusMenu() {
-
+function onItemFocus() {
+	if (!focusItem) onMenuFocus();
+	focusItem = this;
 }
-menuElement.addEventListener("focusin", onMenuFocus);
-/**
- * 
- * @param {{ currentTarget: HTMLButtonElement }} param0 
- */
-function clickMenu({ currentTarget }) { showMenu(menus[currentTarget.tabIndex].list, { element: currentTarget }, { darkStyle: true }) }
+function onItemBlur() { if (keepFocus) { keepFocus = false } else onMenuBlur() }
+function onItemHover() {
+	if (expanded) {
+		keyboardMode = false;
+		this.click();
+	} else if (focusItem) this.focus();
+}
+function onListClose(selected) {
+	this.classList.remove("active");
+	if (keepFocus) {
+		keepFocus = false;
+	} else if (selected) {
+		expanded = false;
+		onMenuBlur();
+	} else {
+		expanded = false;
+		focusItem.focus();
+	}
+}
+/** @this {HTMLButtonElement} */
+function onItemClick(event) {
+	const lastFocus = focusItem;
+	focusItem = this;
+	if (lastFocus) {
+		keepFocus = true;
+		lastFocus.blur();
+	} else onMenuFocus();
+	this.classList.add("active");
+	expanded = true
+	showMenu(menus[this.tabIndex].list, { element: this }, { darkStyle: true, onClose: onListClose.bind(this), keyboardMode: keyboardMode && !event.pointerType });
+	keyboardMode = true;
+}
 function menuItemMapper(item, tabindex) {
-	return ["button", item.text, { class: "menu-item", tabindex, [EVENT_LISTENERS]: [["mouseenter", focusMenu], ["click", clickMenu]] }]
+	return ["button", item.text, {
+		class: "menu-item", tabindex, [EVENT_LISTENERS]: [
+			["mouseenter", onItemHover],
+			["click", onItemClick],
+			["focus", onItemFocus],
+			["blur", onItemBlur]
+		]
+	}]
 }
-function menuMapper2(item) { return ["button", item.title, { class: "menu-option", [EVENT_LISTENERS]: [["click", item.action]] }] }
+
+document.addEventListener("keydown", function (event) {
+	if (event.key != "Alt" || event.ctrlKey || event.shiftKey) return;
+	event.preventDefault();
+	if (focusItem) {
+		if (expanded) closeMenu();
+		focusItem.blur();
+		// @ts-ignore
+	} else menuElement.firstElementChild.focus();
+});
+
 export { menus, renderMenu };
